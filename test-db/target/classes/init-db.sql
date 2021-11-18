2ecd08dea1d9245cc830497f5a9b74393e93b55a
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2021-11-18 20:44:40 +0300

    GIT IGNORE

--- test-db/target/classes/init-db.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO USER (name,login,password,email) VALUES ('Mike','mike','1111','a@mail.ru');
-INSERT INTO USER (name,login,password,email) VALUES ('Tom','tom','1111','a@mail.ru');
-
-INSERT INTO REQUEST (id,groupe,pairs,subject) VALUES (1,'e1','2','math');
-INSERT INTO REQUEST (id,groupe,pairs,subject) VALUES (1,'e2','1','math');
