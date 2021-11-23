7de559f4bb81e59dc0e17f9996a6f43b79a1c16f
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2021-11-23 22:59:14 +0300

    MOCK TEST DAO BEGIN

--- service/src/test/java/com/epam/brest/service/UserServiceImplIT.java
+++ service/src/test/java/com/epam/brest/service/UserServiceImplIT.java
@@ -1,6 +1,5 @@
 package com.epam.brest.service;
 
-
 import com.epam.brest.model.entity.User;
 import com.epam.brest.serviceAPI.UserService;
 import org.apache.logging.log4j.LogManager;
@@ -15,9 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:service-context-test.xml"})
@@ -40,17 +37,19 @@ public class UserServiceImplIT {
         assertTrue(quantity > 0);
         assertEquals(Integer.valueOf(2), quantity);
     }
+
     @Test
-    void shouldAllUsers (){
+    void shouldAllUsers() {
         logger.info("Read All Users {} LIST:");
         List<User> users = userService.getAllUsers();
         assertNotNull(users);
-        if (users.size() > 0){
+        if (users.size() > 0) {
             assertTrue(users.size() == userService.count());
         }
     }
+
     @Test
-    void shouldUser(){
+    void shouldUser() {
         logger.info("Read User {id} ");
         User user = userService.getUserById(1);
         User user1 = userService.getUserById(1);
@@ -58,16 +57,16 @@ public class UserServiceImplIT {
     }
 
     @Test
-    void shouldCreateUser(){
+    void shouldCreateUser() {
         logger.info("Create User {} ");
-        User user = new User("Johnny","johhny","1111","isocrol@yandex.ru");
+        User user = new User("Johnny", "johhny", "1111", "isocrol@yandex.ru");
         Integer count = userService.getAllUsers().size();
         assertTrue(userService.create(user) > count);
         assertTrue(userService.getAllUsers().size() > count);
     }
 
     @Test
-    void shouldUpdateUser(){
+    void shouldUpdateUser() {
         logger.info("Read User {id} ");
         User user = userService.getUserById(1);
         user.setName("oleg");
@@ -76,7 +75,7 @@ public class UserServiceImplIT {
     }
 
     @Test
-    void shouldDeleteUser(){
+    void shouldDeleteUser() {
         logger.info("Delete User {id} ");
         userService.delete(1);
         assertEquals(userService.getUserById(1), null);
