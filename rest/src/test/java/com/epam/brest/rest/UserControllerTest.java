820949b43e8f3ad8759137fac377b3f1e819b668
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2021-12-05 21:44:08 +0300

    TRYED TO DO MOCK PLUS IT TEST IN REST SERVICE

--- rest/src/test/java/com/epam/brest/rest/UserControllerTest.java
+++ rest/src/test/java/com/epam/brest/rest/UserControllerTest.java
@@ -1,7 +1,10 @@
 package com.epam.brest.rest;
 
 
+import com.epam.brest.model.entity.User;
+import com.epam.brest.serviceAPI.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,14 +16,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:app-context-test.xml"})
-public class UserControllerTestTest {
+public class UserControllerTest {
 
     @Autowired
     private UserServiceController userServiceController;
+    @Autowired
+    UserService userService;
 
     ObjectMapper objectMapper = new ObjectMapper();
+
 
     private MockMvc mockMvc;
 
@@ -35,8 +46,17 @@ public class UserControllerTestTest {
 
 
     @Test
-    public void getUserById(){
+    public void shouldGetUserById(){
+        User user = new User(RandomStringUtils.randomAlphabetic(50)
+                , RandomStringUtils.randomAlphabetic(50)
+                ,RandomStringUtils.randomAlphabetic(50)
+                ,RandomStringUtils.randomAlphabetic(50));
+        Integer id = userService.createUserService(user);
 
+        List<User> users = userService.getAllUsersService();
+
+        assertNotNull(users);
+        assertTrue(users.size() > 0);
     }
 
 }
