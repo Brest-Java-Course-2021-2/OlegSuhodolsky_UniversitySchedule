019cacb280fce2b5d0d0efd9b8095d8a1712c161
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2021-12-24 19:41:28 +0300

    SERIALIZATOR TESTS COMPLETED

--- serializator/src/test/java/com/epam/brest/SerializatorTest.java
+++ serializator/src/test/java/com/epam/brest/SerializatorTest.java
@@ -6,21 +6,18 @@ import com.epam.brest.model.entity.Request;
 import com.epam.brest.model.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
-
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.InvalidObjectException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -36,9 +33,9 @@ public class SerializatorTest {
     private Serializator serializator;
 
     List<User> listUser = new ArrayList<>();
-    List<Request> listRequest;
-    List<Admin> listAdmin;
-    List<Groupe> listGroupe;
+    List<Request> listRequest = new ArrayList<>();
+    List<Admin> listAdmin = new ArrayList<>();
+    List<Groupe> listGroupe = new ArrayList<>();
     String fileUser = "user.sql";
     String fileRequest = "request.sql";
     String fileAdmin = "admin.sql";
@@ -50,24 +47,57 @@ public class SerializatorTest {
 
     @Test
     void testFindSerializator() {
-        logger.debug("Execute test: getAll()");
+        logger.debug("Execute test: IsSerializator()");
         assertNotNull(serializator);
 
     }
 
     @Test
     void testUserSerializator() throws InvalidObjectException {
-        logger.debug("Execute test: getAll()");
-        assertNotNull(serializator);
+        logger.debug("Execute test: testUserSerializator()");
         User user = new User("Mike", "mike", "1111", "oleg@mail.com");
         listUser.add(user);
         serializator.serialization(listUser, fileUser);
         List<User> listTest = serializator.deserialization(fileUser);
         User user1 = listTest.get(0);
         assertTrue(user.getName().equals(user1.getName()));
+        assertTrue(user.getLogin().equals(user1.getLogin()));
     }
 
 
+    @Test
+    void testRequestSerializator() throws InvalidObjectException {
+        logger.debug("Execute test: testRequestSerializator()");
+        assertNotNull(serializator);
+        Request request = new Request(2,"e1", "2", "math", new Date());
+        listRequest.add(request);
+        serializator.serialization(listRequest, fileRequest);
+        List<Request> listTest = serializator.deserialization(fileRequest);
+        Request request1 = listTest.get(0);
+        assertTrue(request.getGroupe().equals(request1.getGroupe()));
+    }
+
+    @Test
+    void testAdminSerializator() throws InvalidObjectException {
+        logger.debug("Execute test: testAdminSerializator()");
+        Admin admin = new Admin(1,"Mike", "mike", "1111", "oleg@mail.com");
+        listAdmin.add(admin);
+        serializator.serialization(listAdmin, fileAdmin);
+        List<Admin> listTest = serializator.deserialization(fileAdmin);
+        Admin admin1 = listTest.get(0);
+        assertTrue(admin.getName().equals(admin1.getName()));
+    }
 
 
+    @Test
+    void testGroupeSerializator() throws InvalidObjectException {
+        logger.debug("Execute test: testGroupeSerializator()");
+        assertNotNull(serializator);
+        Groupe groupe = new Groupe("e1");
+        listGroupe.add(groupe);
+        serializator.serialization(listGroupe, fileGroupe);
+        List<Groupe> listTest = serializator.deserialization(fileGroupe);
+        Groupe groupe1 = listTest.get(0);
+        assertTrue(groupe.getNameGroupe().equals(groupe1.getNameGroupe()));
+    }
 }
