fa667362b2cef4eb0c0e93beab9006a69af784a2
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-01-01 18:58:17 +0300

    REST GROUPE WORKING

---
+++ rest/src/main/java/com/epam/brest/rest/GroupeServiceController.java
@@ -0,0 +1,29 @@
+package com.epam.brest.rest;
+
+import com.epam.brest.dao.DaoGroupe;
+import com.epam.brest.model.entity.Groupe;
+import com.epam.brest.serviceAPI.GroupeService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GroupeServiceController {
+
+    private static final Logger logger = LogManager.getLogger(DaoGroupe.class);
+    private final GroupeService groupeService;
+
+    public GroupeServiceController(GroupeService groupeService) {
+        this.groupeService = groupeService;
+    }
+
+    @GetMapping(value = "/groupes")
+    public final List<Groupe> getGroupes() {
+        logger.debug("getGroupes() ");
+        return groupeService.getAllGroupesByNameService();
+    }
+}
---
+++ test-db/src/main/resources/init-app-db.sql
@@ -0,0 +1,8 @@
+INSERT INTO GROUPE (nameGroupe) VALUES ('e1');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e2');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e3');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e4');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e5');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e6');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e7');
+INSERT INTO GROUPE (nameGroupe) VALUES ('e8');
